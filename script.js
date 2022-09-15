369c9b8383634072a0f4913e6f6b5e45bb01a234
lucaslimadesouza <lukxx23121996@gmail.com>
2022-09-14 23:16:12 -0300

    add = horas + minutos

--- script.js
+++ script.js
@@ -1,42 +1,67 @@
-
-
+let hrs = document.getElementById('hrs');
+let min = document.getElementById('min');
+let seg = document.getElementById('seg');
+let mile = document.getElementById('mile');
+let iniciar = document.getElementById('iniciar');
+let parar = document.getElementById('parar');
+let resetar = document.getElementById('resetar');
+let hrss = 00
+let minn = 00
+let segg = 00
 let milee = 00
-let secc = 00
-let mile = document.getElementById("mile");
-let sec = document.getElementById("sec");
-let iniciar = document.getElementById("iniciar");
-let parar = document.getElementById("parar");
-let resetar = document.getElementById("resetar");
-let interval;
+let armazenado
+
 
 iniciar.onclick = () => {
-
-    clearInterval(interval)
-    interval = setInterval(startTimer, 10)
+    clearInterval(armazenado)
+    armazenado = setInterval(contar, 10)
 }
 parar.onclick = () => {
 
-    clearInterval(interval)
+    clearInterval(armazenado)
+
 }
 resetar.onclick = () => {
-    clearInterval(interval)
+    hrss = "00"
+    minn = "00"
+    segg = "00"
     milee = "00"
-    secc = "00"
+    hrs.innerHTML = hrss
+    min.innerHTML = minn
+    seg.innerHTML = segg
     mile.innerHTML = milee
-    sec.innerHTML = secc
 }
 
-function startTimer() {
+function contar() {
     milee++
 
     milee <= 9 ? mile.innerHTML = "0" + milee : false
     milee > 9 ? mile.innerHTML = milee : false
 
     if (milee > 99) {
-        secc++
-        sec.innerHTML = "0" + secc
-        milee = 0
+        segg++
+        milee = "00"
+        mile.innerHTML = milee
+        seg.innerHTML = "0" + segg
+    }
+    segg > 9 ? seg.innerHTML = segg : false
+
+    if (segg > 59) {
+        minn++
+        segg = "00"
+        seg.innerHTML = segg
+        min.innerHTML = "0" + minn
     }
 
-    secc > 9 ? sec.innerHTML = secc : false
+    minn > 9 ? min.innerHTML = minn : false
+
+    if (minn > 59) {
+        hrss++
+        minn = "00"
+        min.innerHTML = minn
+        hrs.innerHTML = "0" + hrss
+    }
+
+    hrss > 9 ? hrs.innerHTML = hrss : false
+
 }
